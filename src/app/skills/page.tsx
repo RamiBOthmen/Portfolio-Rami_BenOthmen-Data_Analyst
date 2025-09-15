4fbdef54cf9135c691cbfa03f765875f84c48ecb
RamiBenOthmen <ramibenothmen15@gmail.com>
2025-09-15 02:41:57 +0100

    Fixed images names for certificates and instructors

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -5,51 +5,51 @@ export default function SkillsPage() {
   const certifications = [
     {
       name: "Google Data Analytics Certificate",
-      image: "../certificates/../certificates/google_data_analysis.png",
+      image: "google_data_analysis.png",
       url: "https://www.credly.com/badges/9e506f5f-a223-43b2-9b57-16d3297a3aee/public_url"
     },
     {
       name: "MTA: Python Programming Fundamentals",
-      image: "../certificates/../certificates/python.png",
+      image: "python.png",
       url: "https://www.credly.com/badges/5d005f88-2707-4bc4-993a-ca1eac590f83"
     },
     {
       name: "SQL Advanced (Hackerrank)",
-      image: "../certificates/sql-a.png",
+      image: "sql-a.png",
       url: "https://www.hackerrank.com/certificates/c15b1ab417f7"
     },
     {
       name: "Microsoft Office Excel Specialist",
-      image: "../certificates/excel.png",
+      image: "excel.png",
       url: "https://drive.google.com/file/d/1tThXoxvRTpurggEY63joP_MtwdzQyvSK/view"
     },
     {
       name: "Microsoft Office Word Specialist",
-      image: "../certificates/word.png",
+      image: "word.png",
       url: "https://drive.google.com/file/d/1CwO2w64X8ILLwIiqkAjoQ-OA6GzXukv-/view"
     },
     {
       name: "Microsoft Office PowerPoint Specialist",
-      image: "../certificates/ppt.png",
+      image: "ppt.png",
       url: "https://drive.google.com/file/d/14-4rzRgd8rFRJVWcInBzuXToZhct5dsu/view"
     },
     {
       name: "Advanced C1 English Certificate (EF SET)",
-      image: "../certificates/ef-set.png",
+      image: "ef-set.png",
       url: "https://cert.efset.org/nSv366"
     },
     {
       name: "Microsoft Azure AI Fundamentals",
-      image: "../certificates/ai.png",
+      image: "ai.png",
       url: "https://www.credly.com/badges/fe102da5-9ea8-4ae8-886b-8e08a0590ce7/linked_in_profile"
     },
     // {
     //   name: "Microsoft Azure Data Fundamentals",
-    //   image: "../certificates/data.png"
+    //   image: "data.png"
     // },
     {
       name: "Agile SCRUM (SCRUM Study)",
-      image: "../certificates/scrum.png",
+      image: "scrum.png",
       url: "https://www.scrumstudy.com/certification/verify?type=SFC&number=955990"
     },
   ];
@@ -87,7 +87,7 @@ export default function SkillsPage() {
                   className="block hover:opacity-80 transition-opacity"
                 >
                   <Image
-                    src={`/assets/projects/${cert.image}`}
+                    src={`/assets/certificates/${cert.image}`}
                     alt={`${cert.name} certificate`}
                     width={200}
                     height={150}
